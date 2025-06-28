4f4e0ba776a4836d2688c167b8212cc59d295e67
KUNALP1607 <kunalp1604@gmail.com>
2025-06-28 23:05:16 +0530

    Updated Navbar, Home, App; added Login screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,9 +5,9 @@ import Footer from '../components/Footer'
 export default function Home() {
   return (
     <div>
-        <div><Navbar/></div>
+        <div> <Navbar/> </div>
         <div>Body</div>
-        <div><Footer></Footer></div>
+        <div> <Footer></Footer> </div>
 
     </div>
   )
