7eb0eff8dbc346ebebd95838effeadcd7a22b4f2
KUNALP1607 <kunalp1604@gmail.com>
2025-06-30 22:30:41 +0530

    Card Component created

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,30 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
 
 export default function Home() {
   return (
     <div>
       <div> <Navbar /> </div>
-
-      <div>
-        <div className="card mt-3" style={{"width": "18rem", "maxHeight": "360px"}}>
-          <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is some Important text.</p>
-              <div className='container' w-100>
-                <select className='m-2 h-100 w-100' style={{backgroundColor: '#20c997'}}>
-                  {Array.from(Array(6), (i)=>{
-                    return(
-                      <option key={i+1} value={i+1}> {i+1} </option>
-                    )
-                  })}
-                </select>
-              </div>
-            </div>
-        </div>
-      </div>
+      <div><Card /></div>
       <div> <Footer></Footer> </div>
 
     </div>
