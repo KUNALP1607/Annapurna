be942a77468b16173af394b6bf48d6e78c2d084e
KUNALP1607 <kunalp1604@gmail.com>
2025-07-03 15:17:57 +0530

    Added Carousel, updated Footer, installed Bootstrap

---
+++ src/components/Carousel.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export default function Carousel() {
+  return (
+    <div>
+      <div
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="https://images.unsplash.com/photo-1547584370-2cc98b8b8dc8?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Burger"
+              className="d-block w-100"
+              style={{ height: '400px', objectFit: 'cover' }}
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Pizza"
+              className="d-block w-100"
+              style={{ height: '400px', objectFit: 'cover' }}
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://images.unsplash.com/photo-1606491956689-2ea866880c84?q=80&w=1021&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3DD"
+              alt="Pastry"
+              className="d-block w-100"
+              style={{ height: '400px', objectFit: 'cover' }}
+            />
+          </div>
+        </div>
+
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+}
