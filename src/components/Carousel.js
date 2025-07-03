c4cabc1d421aaa0d14a311a56dd1ab584b30039f
KUNALP1607 <kunalp1604@gmail.com>
2025-07-03 15:44:06 +0530

    Carousel Image sizes changed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -14,7 +14,7 @@ export default function Carousel() {
               src="https://images.unsplash.com/photo-1668236543090-82eba5ee5976?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Burger"
               className="d-block w-100"
-              style={{ height: '400px', objectFit: 'cover' }}
+              style={{ height: '600px', objectFit: 'cover' }}
             />
           </div>
           <div className="carousel-item">
@@ -22,7 +22,7 @@ export default function Carousel() {
               src="https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Pizza"
               className="d-block w-100"
-              style={{ height: '400px', objectFit: 'cover' }}
+              style={{ height: '600px', objectFit: 'cover' }}
             />
           </div>
           <div className="carousel-item">
@@ -30,7 +30,7 @@ export default function Carousel() {
               src="https://images.unsplash.com/photo-1606491956689-2ea866880c84?q=80&w=1021&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3DD"
               alt="Pastry"
               className="d-block w-100"
-              style={{ height: '400px', objectFit: 'cover' }}
+              style={{ height: '600px', objectFit: 'cover' }}
             />
           </div>
         </div>
